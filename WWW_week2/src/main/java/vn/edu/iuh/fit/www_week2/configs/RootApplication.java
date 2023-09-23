040725687a8025c1354de23b2dc76e57fefbc9ea
NgoNhatThai <ngothai9a1@gmail.com>
2023-09-23 19:37:28 +0700

    push WWW week 2

---
+++ WWW_week2/src/main/java/vn/edu/iuh/fit/www_week2/configs/RootApplication.java
@@ -0,0 +1,4 @@
+package vn.edu.iuh.fit.www_week2.configs;
+
+public class RootApplication {
+}
---
+++ WWW_week2/src/main/java/vn/edu/iuh/fit/www_week2/controllers/CreateOrderController.java
@@ -0,0 +1,4 @@
+package vn.edu.iuh.fit.www_week2.controllers;
+
+public class CreateOrderController {
+}
