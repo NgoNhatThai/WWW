64526a26c1920c45162b39014031bb9ec93c7d19
NgoNhatThai <ngothai9a1@gmail.com>
2023-11-25 11:01:45 +0700

    connect H2 database

--- src/main/java/com/vn/edu/iuh/fit/www_lab8/configs/SecurityConfig.java
+++ src/main/java/com/vn/edu/iuh/fit/www_lab8/configs/SecurityConfig.java
@@ -7,18 +7,41 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+//    @Autowired
+//    public void globalConfig(AuthenticationManagerBuilder auth, PasswordEncoder
+//            encoder)throws Exception{
+//        auth.inMemoryAuthentication()
+//                .withUser(User.withUsername("admin")
+//                        .password(encoder.encode("admin"))
+//                        .roles("ADMIN")
+//                        .build())
+//                .withUser(User.withUsername("thai")
+//                        .password(encoder.encode("thai"))
+//                        .roles("THAI")
+//                        .build())
+//                .withUser(User.withUsername("user")
+//                        .password(encoder.encode("user"))
+//                        .roles("USER")
+//                        .build())
+//        ;
+//    }
     @Autowired
     public void globalConfig(AuthenticationManagerBuilder auth, PasswordEncoder
-            encoder)throws Exception{
-        auth.inMemoryAuthentication()
+            encoder, DataSource dataSource)throws Exception{
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
                 .withUser(User.withUsername("admin")
                         .password(encoder.encode("admin"))
                         .roles("ADMIN")
@@ -42,6 +65,8 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
 
         );
+        http.csrf(csrf->csrf.ignoringRequestMatchers("/h2-console/**"));
+        http.headers(headers->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
